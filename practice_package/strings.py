9bba2f31aa38d15809387a241c325418290f8363
Anton Popov <144782505+Popohka05@users.noreply.github.com>
2025-04-25 03:53:34 +0300

    Update strings.py

--- practice_package/strings.py
+++ practice_package/strings.py
@@ -1,1 +1,14 @@
-pass
+def extract_file_name(full_file_name):
+    pass
+
+def encrypt_sentence(sentence):
+    pass
+
+def check_brackets(expression):
+    pass
+
+def reverse_domain(domain):
+    pass
+
+def count_vowel_groups(word):
+    pass
