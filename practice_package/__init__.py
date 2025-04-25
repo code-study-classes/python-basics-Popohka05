6d2d09ce82bc6ee054893cd9c7b9a348573ea9c7
Anton Popov <144782505+Popohka05@users.noreply.github.com>
2025-04-25 04:13:14 +0300

    Update __init__.py

--- practice_package/__init__.py
+++ practice_package/__init__.py
@@ -1,39 +1,6 @@
-from .boolean import (
-    check_ascending_digits,
-    check_between_numbers,
-    check_odd_three,
-    check_palindrome_number,
-    check_unique_digits,
-)
-from .numbers import (
-    calculate_digit_sum,
-    calculate_distance,
-    calculate_rect_area,
-    calculate_segments,
-    round_to_multiple,
-)
-from .strings import (
-    check_brackets,
-    count_vowel_groups,
-    encrypt_sentence,
-    extract_file_name,
-    reverse_domain,
-)
+# from numbers import calculate_distance
 
-__all__ = [
-    'calculate_distance',
-    'calculate_segments',
-    'calculate_digit_sum',
-    'calculate_rect_area',
-    'round_to_multiple',
-    'extract_file_name',
-    'encrypt_sentence',
-    'check_brackets',
-    'reverse_domain',
-    'count_vowel_groups',
-    'check_between_numbers',
-    'check_odd_three',
-    'check_unique_digits',
-    'check_palindrome_number',
-    'check_ascending_digits'
-]
+# __all__ = (
+#     "calculate_distance"
+# )
+pass
